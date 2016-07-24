69deb0a0c6f5f000e3b6c56160f5e9eba04d8ee7
Jeff McGee <JeffAMcGee@gmail.com>
2016-07-24 13:56:31 -0700

    use react-bootstrap

--- ballpit/config/webpack.config.dev.js
+++ ballpit/config/webpack.config.dev.js
@@ -58,7 +58,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        include: srcPath,
+        include: [srcPath, nodeModulesPath],
         loader: 'style!css!postcss'
       },
       {
--- ballpit/src/App.js
+++ ballpit/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Row, Col } from 'react-bootstrap';
+
+import ExoNavbar from './ExoNavbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ExoNavbar />
+        <Grid fluid={true}>
+          <Row>
+            <Col>
+              <h2>Hello worlds!</h2>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
